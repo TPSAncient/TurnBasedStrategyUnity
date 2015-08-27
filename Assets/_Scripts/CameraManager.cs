4c024c0b4a984d0eb6a0689a5780fae45c562e4e
herki Pärn <herki77@gmail.com>
2015-08-27 20:53:47 +0100

    Added selection and dummy data for objects

---
+++ Assets/_Scripts/CameraManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SelectObject();
+    }
+
+    private void SelectObject()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                print("Hit something");
+                SelectedObject selected = hit.transform.GetComponent<SelectedObject>();
+                print(selected.Id);
+                print(selected.Name);
+                print(hit.point);
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/SelectedObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectedObject : MonoBehaviour {
+    public int Id = 0;
+    public string Name = "";
+    public SelectedObjectType SelectedObjectType = SelectedObjectType.None;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
